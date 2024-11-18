8480e2ab473b180f5e65cab8f955b3398878c400
송재욱 <haensol976520@gmail.com>
2024-11-18 23:50:14 +0900

    add-page

--- src/app/Router.js
+++ src/app/Router.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Intro from '../pages/Intro/Index.jsx';
+import Intro from '../pages/intro/Index.jsx';
 import Signin from '../pages/signin/Index.jsx';
-import Home from '../pages/Home/Index.jsx';
-
+import Home from '../pages/home/Index.jsx';
+import Profile from '../pages/profile/Index.jsx';
+import Reminder from '../pages/reminder/Index.jsx';
 const Router = () => {
   return (
     <BrowserRouter>
@@ -11,6 +12,8 @@ const Router = () => {
         <Route path="/" element={<Intro />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/reminder" element={<Reminder />} />
       </Routes>
     </BrowserRouter>
   );
