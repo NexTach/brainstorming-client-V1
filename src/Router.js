19006f7c23b38ef384ce014ce3cb7d9c976717d8
송재욱 <haensol976520@gmail.com>
2024-11-18 16:45:21 +0900

    FSD

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Main from './pages/Main/Main';
+import Login from './pages/Login/login';
+import Home from './pages/Home/home';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/main" element={<Main />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
