6a52cce882955d722272e564c75e6a4d3e39d37e
송재욱 <haensol976520@gmail.com>
2024-11-18 22:32:22 +0900

    globalstyle

--- src/widgets/navigation/Index.jsx
+++ src/widgets/navigation/Index.jsx
@@ -3,10 +3,16 @@ import styled from 'styled-components';
 import ReminderIcon from '../../shared/assets/icon/ReminderIcon';
 import HomeIcon from '../../shared/assets/icon/HomeIcon';
 import ProfileIcon from '../../shared/assets/icon/ProfileIcon';
+import { Colors } from '../../shared/ui/Colors';
 
 const Container = styled.nav`
   display: flex;
-  gap: 1rem;
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  padding: 1rem;
+  background-color: ${Colors.WHITE};
+  justify-content: space-around;
 `;
 
 export const Navigation = () => {
