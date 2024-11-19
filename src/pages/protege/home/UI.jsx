57883c6d0067865b3bc37cd67fd122daf5b1afbd
송재욱 <haensol976520@gmail.com>
2024-11-19 22:42:00 +0900

    title-date

--- src/pages/protege/home/UI.jsx
+++ src/pages/protege/home/UI.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Colors } from '../../../shared/UI/Colors';
 
 const HomeWrapper = styled.div``;
 const HomeContent = styled.div``;
-const HomeHeader = styled.div``;
+const HomeHeader = styled.div`
+  font-size: 1.5rem;
+  padding: 2rem;
+  color: ${Colors.BLACK};
+  span {
+    color: ${Colors.MAIN_COLOR};
+  }
+`;
 
 const UI = () => {
   const user = 'user';
@@ -11,11 +19,14 @@ const UI = () => {
     <HomeWrapper>
       <HomeContent>
         <HomeHeader>
-          안녕하세요, <span>{user}</span>님! <br />
+          안녕하세요, {user}님! <br />
           오늘은 {new Date().getFullYear()}년 {new Date().getMonth() + 1}월{' '}
           {new Date().getDate()}일{' '}
-          {['일', '월', '화', '수', '목', '금', '토'][new Date().getDay()]}
-          요일입니다!
+          <span>
+            {['일', '월', '화', '수', '목', '금', '토'][new Date().getDay()]}
+            요일
+          </span>
+          이에요!
         </HomeHeader>
       </HomeContent>
     </HomeWrapper>
