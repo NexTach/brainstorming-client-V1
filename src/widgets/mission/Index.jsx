6d400e62b6e2b123177099bb1b5edde9ae8af98f
송재욱 <haensol976520@gmail.com>
2024-11-19 23:05:27 +0900

    innertext

--- src/widgets/mission/Index.jsx
+++ src/widgets/mission/Index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import MissionHeader from '../mission/MissionHeader';
 import MissionWrapper from '../mission/MissionWrapper';
 
-const Mission = () => {
+const Mission = ({ innerText }) => {
   return (
     <MissionWrapper>
-      <MissionHeader>오늘의 미션 리스트</MissionHeader>
+      <MissionHeader>{innerText}</MissionHeader>
     </MissionWrapper>
   );
 };
