86626b10e83acb7eeeb1ec435fc4878d588dd413
송재욱 <haensol976520@gmail.com>
2024-11-27 09:37:47 +0900

    del

--- src/shared/api/ChatGPT.jsx
+++
@@ -1,2 +0,0 @@
-export const CHATGPT_API_KEY =
-  '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';
